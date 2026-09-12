50f3afb9cf9d9addc28a57bc24574bab743cb26c
Noah Gabel <noahga2009@icloud.com>
2026-09-12 20:45:34 +0200

    removes testing amount in game_manager

--- src/game_manager.py
+++ src/game_manager.py
@@ -20,8 +20,7 @@ class Manager():
             self.difficulty = difficulty
             self.total_score: int = 0
 
-            # TODO remove the amount only for testing
-            city_data = self.database.get_random_cities(difficulty, amount=10)
+            city_data = self.database.get_random_cities(difficulty)
             self.cities: list[City] = [City(city_ONR=city_ONR,name=name, coords=Coordinates(lat=latitude, lon=longitude)) for (city_ONR, name, latitude, longitude) in city_data]
 
             self.results: list[Guess] = []
